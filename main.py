12fe0b2cd6aef195d34681cf20c74ab21f39200f
MicahHolt2022 <mccormickprophet@gmail.com>
2021-09-28 09:32:39 -0500

    First Attempt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def close10(a,b):
+    if abs((10 - a)) > abs((10 - b)):
+        return b
+    elif abs((10 - b)) > abs((10 - a)):
+        return a
+    else:
+        return "0"
+
+print(close10(8,11))
